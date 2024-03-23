b75c76239e911141d97822d7a410413d6de3f1c9
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-03-23 19:25:32 +0530

    Fix start script and add .gitignore file

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.Port || 5000;
-app.get("api/contacts",(req,res) => {
-    res.send("get all contacts ");
+
+app.get("api/contacts", (req, res) => {
+  res.send("get all contacts ");
 });
 
-app.listen(port , () =>{
-    console.log('Server running on port ${port}');
+app.listen(port, () => {
+  console.log("Server running on port ${port}");
 });
